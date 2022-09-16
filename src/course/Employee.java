41bae5d7025eef74057d9c0567caeae8aea3cff4
vadimkosolapov <vilgutis@icloud.com>
2022-09-16 12:11:33 +0500

    done Easy

---
+++ src/course/Employee.java
@@ -0,0 +1,56 @@
+package course;
+
+public class Employee {
+    private String name;
+    private String middleName;
+    private String lastName;
+
+    private int division;
+    private int salary;
+
+    public static int employeeCounter = 0;
+
+    private int id;
+
+    public Employee(String name, String middleName, String lastName, int division, int salary) {
+        this.name = name;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.division = division;
+        this.salary = salary;
+        this.id = employeeCounter++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getDivision() {
+        return division;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setDivision(int division) {
+        this.division = division;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Имя: " + name + " " + "Отчество: " + middleName + " " + "Фамилия: " +lastName + " " + "Подразделение: " + division + " " + "Зарплата: " + salary;
+    }
+}
--- src/course/Main.java
+++ src/course/Main.java
@@ -1,7 +1,78 @@
 package course;
 
+import java.util.Arrays;
+
 public class Main {
+    private static Employee[] employees = new Employee[10];
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Employee emp= new Employee("Иван", "Иванович", "Иванов", 3, 21000);
+        Employee emp1 = new Employee("Пётр", "Сергеевич", "Клабуков", 1, 19000);
+        Employee emp2 = new Employee("Василий", "Сергеевич", "Мутко", 1, 25000);
+        Employee emp3 = new Employee("Денис", "Владимирович", "Зуборев", 4, 29000);
+        Employee emp4 = new Employee("Софья", "Констатиновна", "Мутко", 1, 69000);
+        Employee emp5 = new Employee("Анна", "Алексеевна", "Ницше", 3, 45000);
+        Employee emp6 = new Employee("Александр", "Алексеевич", "Притуляк", 2, 37000);
+        Employee emp7 = new Employee("Александр", "Петрович", "Шолгин", 1, 11000);
+        Employee emp8 = new Employee("Артур", "Захарович", "Липатов", 4, 17000);
+        Employee emp9 = new Employee("Альберт", "Александрович", "Грабурдо", 1, 50000);
+
+        employees[0] = emp;
+        employees[1] = emp1;
+        employees[2] = emp2;
+        employees[3] = emp3;
+        employees[4] = emp4;
+        employees[5] = emp5;
+        employees[6] = emp6;
+        employees[7] = emp7;
+        employees[8] = emp8;
+        employees[9] = emp9;
+
+        printEmployees();
+        System.out.println(findMinSalary());
+        System.out.println(findMaxSalary());
+        System.out.println(averageSalary());
+    }
+//
+    public static void printEmployees() {
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println(employees[i]);
+        }
+    }
+
+    public static int printSum() {
+        int sum = 0;
+        for (int i = 0; i < employees.length; i++) {
+            sum += employees[i].getSalary();
+        }
+        return sum;
+    }
+    public static int findMinSalary() {
+        int minSalary = employees[0].getSalary();
+        for (int i = 0; i < employees.length; i++) {
+            if (minSalary > employees[i].getSalary()) {
+                minSalary = employees[i].getSalary();
+            }
+        }
+        return minSalary;
+    }
+    public static int findMaxSalary() {
+        int maxSalary = employees[0].getSalary();
+        for (int i = 0; i < employees.length; i++) {
+            if (maxSalary < employees[i].getSalary()) {
+                maxSalary = employees[i].getSalary();
+            }
+        }
+        return maxSalary;
+    }
+    public static int averageSalary() {
+        int average = 0;
+        for (int i = 0; i < employees.length; i++) {
+            average += employees[i].getSalary();
+
+            }
+        return average/employees.length;
     }
 }
+
+
